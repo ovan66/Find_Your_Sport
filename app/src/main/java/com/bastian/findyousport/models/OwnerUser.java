e646b00b90ae178b0e24a89aac1cd33e9a309540
Bastian <bstian23@gmail.com>
2016-12-19 20:44:13 -0300

    Add: owner profile

---
+++ app/src/main/java/com/bastian/findyousport/models/OwnerUser.java
@@ -0,0 +1,94 @@
+package com.bastian.findyousport.models;
+
+import java.io.Serializable;
+
+/**
+ * Created by Bastian on 19-12-2016.
+ */
+
+public class OwnerUser implements Serializable{
+
+    private String uid, nameLocal, location, email, facebook, key, category;
+    private int phoneNum;
+
+
+    public OwnerUser() {
+    }
+
+
+    public OwnerUser(String uid, String nameLocal, String location, String email, String facebook, String key, String category, int phoneNum) {
+        this.uid = uid;
+        this.nameLocal = nameLocal;
+        this.location = location;
+        this.email = email;
+        this.facebook = facebook;
+        this.key = key;
+        this.category = category;
+        this.phoneNum = phoneNum;
+
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getNameLocal() {
+        return nameLocal;
+    }
+
+    public void setNameLocal(String nameLocal) {
+        this.nameLocal = nameLocal;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFacebook() {
+        return facebook;
+    }
+
+    public void setFacebook(String facebook) {
+        this.facebook = facebook;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(int phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+}
---
+++ app/src/main/java/com/bastian/findyousport/views/owner/OwnerCreateActivity.java
@@ -0,0 +1,15 @@
+package com.bastian.findyousport.views.owner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bastian.findyousport.R;
+
+public class OwnerCreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owner_create);
+    }
+}
