b2567c15f40b868bb3aafde9ed5f02dae9362dae
cutiko <cutiko@gmail.com>
2016-12-28 15:15:34 -0300

    WIP: profiles list and list item

---
+++ app/src/main/java/com/bastian/findyousport/views/profiles/ProfilesActivity.java
@@ -0,0 +1,15 @@
+package com.bastian.findyousport.views.profiles;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.bastian.findyousport.R;
+
+public class ProfilesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profiles);
+    }
+}
